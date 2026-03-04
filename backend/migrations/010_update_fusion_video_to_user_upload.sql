8b4ad58dbe795ea6e7d9cfce63ccf5ca6ba6d81a
Thomas Sorensen <tsorensen007@gmail.com>
2026-03-05 00:47:01 +0100

    fix: remove migrations

--- backend/migrations/010_update_fusion_video_to_user_upload.sql
+++
@@ -1,7 +0,0 @@
--- Use the user-uploaded fvessel video for Mock data tab.
--- S3 key from: videos/private/default-group/.../manual/2022_05_10_19_22_05_2022_05_10_19_25_04_b.mp4
-
-UPDATE media_assets
-SET s3_key = 'videos/private/default-group/HuWS2pHe8cZeuro8llQ2bGOdacTv4GsJ/manual/2022_05_10_19_22_05_2022_05_10_19_25_04_b.mp4',
-    updated_at = NOW()
-WHERE asset_name = 'fusion_video';
