331461b0a1347993c7005edee40b964ae441f70d
Ludvig Øvrevik <iluddeludde@gmail.com>
2026-03-15 22:12:16 +0100

    feat: add Configure page with tabbed layout for media library and toolbar config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,10 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "@ocean-industries-concept-lab/openbridge-webcomponents/dist/openbridge.css";
 import { ObcBrillianceMenu } from "@ocean-industries-concept-lab/openbridge-webcomponents-react/components/brilliance-menu/brilliance-menu";
 import "./App.css";
-import MediaLibrary from "./pages/media-library/MediaLibrary";
 import Ais from "./pages/Ais";
 import Components from "./pages/Components";
 import AROverlay from "./pages/AROverlay";
-import ControlCustomization from "./pages/ControlCustomization";
+import Configure from "./pages/Configure";
 import { useClock } from "./hooks/useClock";
 import { useNavigation } from "./hooks/useNavigation";
 import { AppTopBar } from "./components/app/AppTopBar";
@@ -125,9 +124,8 @@ function App() {
             element={<AROverlay onAuthGateVisibleChange={handleAuthGateVisibleChange} />}
           />
           <Route path="/ais" element={<Ais />} />
-          <Route path="/media-library" element={<MediaLibrary />} />
           <Route path="/components" element={<Components />} />
-          <Route path="/control-customization" element={<ControlCustomization />} />
+          <Route path="/control-customization" element={<Configure />} />
           <Route path="/" element={<Navigate to="/ar" replace />} />
           <Route path="*" element={<Navigate to="/ar" replace />} />
         </Routes>
